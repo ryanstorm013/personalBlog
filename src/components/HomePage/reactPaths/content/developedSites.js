040a20276ada6d23e87b75867e9248b74f47b626
ryanstorm <ryanstorm013@gmail.com>
2023-11-18 19:01:10 -0800

    Image edit

--- src/components/HomePage/reactPaths/content/developedSites.js
+++ src/components/HomePage/reactPaths/content/developedSites.js
@@ -1,23 +1,23 @@
 const sites = [
     {
         id: 1,
-        image: "./src/components/images/simonGame.PNG",
+        image: "../src/components/images/simonGame.png",
         title: "Simon Game"
     },
     {
         id: 2,
-        image: "./src/components/images/passwordGenerator.PNG",
+        image: "../src/components/images/passwordGenerator.png",
         title: "Password Generator"
     },
     {
         id: 3,
-        image: "./src/components/images/toDoList.PNG",
+        image: "../src/components/images/toDoList.png",
         title: "To do List"
         
     },
     {
         id: 4, 
-        image: "./src/components/images/lazyloader.PNG",
+        image: "../src/components/images/lazyloader.png",
         title: "LazyLoader News"
     }
 
