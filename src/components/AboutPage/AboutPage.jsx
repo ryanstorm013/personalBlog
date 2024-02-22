2b3eaaf687f569c84087c38ef0a1696786271f2f
ryanstorm <ryanstorm013@gmail.com>
2024-02-21 20:38:27 -0800

    Fixing Multi Pages

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,3 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <React.StrictMode>
+//         <h1>Taco Bell</h1>
+//     </React.StrictMode>
+//   )
+
 
 export default function AboutPage() {
     return (
