7c79afe1ece60dce0f92258a97a5de77da47d7a7
ryanstorm <ryanstorm013@gmail.com>
2024-02-21 19:57:53 -0800

    Fixing Multi Pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve, dirname } from 'path';
+import { fileURLToPath  } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/personalBlog",
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, './src/components/HomePage/HomePage.jsx')
-  //     }
-  //   }
-  // }
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, './src/components/AboutPage/AboutPage.jsx')
+      }
+    }
+  }
 });
