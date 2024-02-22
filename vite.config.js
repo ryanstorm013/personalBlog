9544985d37af1ac01a7c2502d896b971d96760f1
ryanstorm <ryanstorm013@gmail.com>
2024-02-21 20:56:00 -0800

    Fixing Multi Pages

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,24 @@ const __dirname = dirname(__filename);
 export default defineConfig({
   base: "/personalBlog",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about', 'index.html')
-      }
+  pages: {
+    index: {
+      entry: 'main.jsx',
+      template: 'index.html',
+      filename: 'index.html'
+    },
+    about: {
+      entry: './src/components/AboutPage/AboutPage.jsx',
+      template: './src/components/AboutPage/about.html',
+      filename: 'about.html'
     }
   }
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, 'index.html'),
+  //       about: resolve(__dirname, 'about', 'index.html')
+  //     }
+  //   }
+  // }
 });
